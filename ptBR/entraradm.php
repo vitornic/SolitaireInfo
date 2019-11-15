05fbe9899c09585c49aba52b8c94d3bcc027a1b4
João Vitor <34252083+vitornic@users.noreply.github.com>
2019-11-14 21:26:01 -0300

    adm

---
+++ ptBR/entraradm.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="icon" type="image/png" href="../images/fav.png">
+    <meta charset="utf-8" name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="../css/stylesheet.css">
+    <title>Login</title>
+</head>
+
+<body>
+    <div class="nav">
+        <form method="POST" action="pesquisa.php">
+            <ul>
+                <a href="inicio.php"><img src="../images/thumbnail_Logo Solitarie.png"></a>
+                <input name="pesq" id="pesq" type="text" placeholder="PESQUISAR">
+                <input type="submit" id="lupa" value="      ">
+
+                <div class="dropdown">
+                    <button class="dropbtn"><a href="#">Traduzir</a></button>
+                    <div class="dropdown-content">
+                        <a href="../enUS/login.php">EN-US</a>
+                    </div>
+                </div>
+                <?php
+                    if(isset($_COOKIE["NomeADM"]) and (isset($_COOKIE["EmailADM"]))) {
+                        $login_cookie = $_COOKIE['NomeADM'];
+                        echo "<li><a href='logout.php'>Sair</a></li>";
+                        echo "<li><a href='perfil.php'>Olá, $login_cookie</a></li>";
+                    } else {
+                        echo '<li><a href="entrar.php">Login</a></li>';
+                        echo '<li><a href="cadastro.php">Cadastro</a></li>';
+                    }
+                ?>
+                <li><a href="formulario.php">Formulário</a></li>
+            </ul>
+        </form>
+    </div>
+
+    <div class="sidebar">
+        <ul>
+            <strong>CATEGORIAS</strong>
+            <li><a href="computadores.php">Computadores</a></li>
+            <li><a href="perifericos.php">Periféricos</a></li>
+            <li><a href="acessorios.php">Acessórios</a></li>
+            <li><a href="jogos.php">Jogos</a></li>
+            <li><a href="hardware.php">Armazenamento</a></li>
+            <li><a href="video.php">Vídeo</a></li>
+            <li><a href="audio.php">Áudio</a></li>
+            <li><a href="gabinetes.php">Gabinetes</a></li>
+            <li><a href="redes.php">Redes</a></li>
+            <?php
+                    if(isset($_COOKIE["NomeADM"]) and (isset($_COOKIE["EmailADM"]))) {
+                        $login_cookie = $_COOKIE['NomeADM'];
+                        echo "<li><hr></li>";
+                        echo "<li><a class='notmarked'>Administrativos</a></li>";
+                        echo "<li><a href='perfil.php'>Olá, $login_cookie</a></li>";
+                        echo "<li><a href='cadastrodeproduto.php'>Cadastro de Produto</a></li>";
+                        echo "<li><a href='cadastrodetipo.php'>Cadastro de Tipo</a></li>";
+                        echo "<li><a href='logout.php'>Sair</a></li>";
+                    }    
+                ?>
+        </ul>
+    </div>
+
+    </body>
+</html>
+
+<?php
+    @$login_cookie = $_COOKIE['EmailADM'] and $_COOKIE['SenhaADM'];
+    if(isset($login_cookie)){
+        echo"<br>";
+        echo "<div class='boxform'>";
+        echo"Bem-Vindo, ".$login_cookie." <br>";
+        echo"Você já está <font color='green'>LOGADO</font>";
+        echo "</div>";
+    } else {
+        echo "<br>";
+        echo "<form method='POST' action='login.php' class='boxform' accept-charset='UTF-8'>
+                <h1>login cliente</h1>
+                <input placeholder='Email' type='Email' name='emailADM' required='true' maxlength='100' pattern='[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$' title='exemple@exemple.com'>
+                <input placeholder='Senha' type='password' size='20' name='senhaADM' required = 'true'>
+                <input type='submit' value='ENTRAR' name='entrar'>
+                <input type='reset' value='LIMPAR'>
+                <h4><a href='#'>Esqueceu a senha ?</a></h4>
+                <h4><a href='cadastro.php'>Não tem um cadastro ?</h4>
+            </form>";
+    }
+    ?>
