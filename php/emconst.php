1adf118b8cf1bf7542be0e4cf65371e5a6c02fb5
vitornic <joaov.nicolette@gmail.com>
2019-11-18 00:56:44 -0300

    oo

--- php/emconst.php
+++ php/emconst.php
@@ -1,1 +1,15 @@
+<!DOCTYPE html>
+<head>
+    <title>REDIRECIONANDO EM 5 SEGUNDOS...</title>
+</head>
+
+<body>
 <img src="../images/emconst.png" width="100%" height="100%">
+
+<?php
+    $paginaAtual = $_SERVER['HTTP_REFERER'];
+
+    header("refresh: 5;$paginaAtual");
+?>
+
+</body>
